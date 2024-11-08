b717925725dfd7eedb96e3e503d78d9c730ecb2a
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-08 14:30:19 +0530

    conditionl

---
+++ Conditionl.py
@@ -0,0 +1,34 @@
+'''
+Conditional statement(decision making)(if-else)
+control flow statement(looping)
+
+1)if-else
+2)if-else iader(more then)
+if-else nesting
+'''
+# age = int (input("enter age:"))
+
+
+# if age>=18:
+#     print("eligible")
+# else:
+#  print("not ")
+# =====================================
+# print("main script")
+# if False: 
+#          print("if body1")
+#          print("if body2")
+#          print("if body3")
+#          print("if body4")
+# else:
+#  print("else 1")
+#  print("else 2")
+
+ch = input("enter any character:")
+V="AEIOUaeiou"
+
+if ch in V:
+    print("vowel")
+else:
+    print("not")
+
