ea932606709690c08833526c69413d6c7c89d9fe
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-11 13:52:01 +0530

    datatype

---
+++ datatype.py
@@ -0,0 +1,17 @@
+# Data-Type
+
+#numerals      sequence          Mapping         Set          none
+#int            list             dictionary      set          None
+#float          tuple
+#complex        string
+#bool
+
+
+#input operation 
+#number1 = int(input("enter 1st number:"))
+#number2 =int(input("enter 2nd number:"))
+#print("sum",number1+number2)
+
+num1 = (input("enter your name "))
+num2 = (input("enter your sarname "))
+print("Full Name",num1+num2)
