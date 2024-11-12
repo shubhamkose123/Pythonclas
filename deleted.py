a16ba62e7001f002964d42babeac8f7c230b87cb
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-12 12:25:37 +0530

    Add files via upload

---
+++ deleted.py
@@ -0,0 +1,26 @@
+#pop()
+#delete last element
+lis1=['ajay','shubham','danish','ankur']
+print(lis1)
+deleted_value=lis1.pop(1)
+print(lis1)
+print(deleted_value)
+# =============ye none print krega============================
+lis1=['ajay','shubham','danish','ankur']
+a=lis1.append("xyz")
+print(a)
+
+#=====================remove()======================================
+lis1=['ajay','shubham','danish','ankur']
+lis1.remove('danish')
+print(lis1)
+
+#=======================clear()=====================================
+lis1=['ajay','shubham','danish','ankur']
+lis1.clear()
+print(lis1)
+
+#=================delete ================================
+lis1=['ajay','shubham','danish','ankur']
+del lis1
+print(lis1)
