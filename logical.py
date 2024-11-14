b95c44dae5c708813d15fb9a060f093e7e271ca4
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-14 12:20:43 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,14 @@
+print(True and True)
+print(3>2 and 34<5)
+print(3>2 or 34<5)
+print(not(24>56))
+
+#membership operetor
+#in ,not in
+string1 = "apple is good 4 health"
+print("is"in string1)
+print('si'in string1)
+print("apple  is" in string1)
+print("4" in string1)
+print("health " in string1)
+print("health " not in string1)
