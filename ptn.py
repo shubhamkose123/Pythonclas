714389002946d068b6e975611930014b8d7fbf19
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-26 12:39:39 +0530

    Add files via upload

---
+++ ptn.py
@@ -0,0 +1,62 @@
+# n=int(input("Enter row no:"))
+# i=1
+# while i<=n:
+#     print('*'*i+(n-i)* ' ')
+#     i=i+1
+# ===========================================
+# n=int(input("Enter row no:"))
+# i=0
+# while i<=n:
+#     print((n-i)*'*'+''*i)
+#     i=i+1
+#=====================================   
+# n=int(input("Enter row no:"))
+# i=0
+# while i<=n:
+#     print(' '*i+'*'*(n-i))
+#     i=i+1
+#====================================
+# n=int(input("Enter row no:"))
+# i=1
+# while i<=n:
+#     print((n-i)*' '+i*' *')
+#     i=i+1
+# ===================================
+# n=(int(input("Enter row=")))
+# i=1
+# while i<=n:
+
+#     print('* '*i)
+#     i=i+1
+# i=i-2
+# while i>0:
+#     print('* '*i)
+#     i=i-1  
+# ===================================================
+# n=(int(input("Enter row=")))
+# i=1
+# while i<=n:
+
+#     print(' '*(n-i)+'* '*i)
+#     i=i+1
+# i=i-2      #dimond
+# while i>0:
+#      print((n-i)*' '+'* '*i)
+#      i=i-1  
+# ======================================================
+# n=(int(input("Enter row=")))
+# i=n
+# while i>0:
+
+#     print('*'*i)
+#     i=i-1
+# i=i+2  
+# while i<=n:
+#      print('*'*i)
+#      i=i+1
+# =================================================
+n=(int(input("Enter row=")))
+i=1
+while i<=n:
+    print(' ' * (n-i)+'*'*i)
+    i=i+1
