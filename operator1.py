8826d53fe4d7a548db884a0fd2c4319b4c7095d6
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-29 12:44:01 +0530

    Add files via upload

---
+++ operator1.py
@@ -0,0 +1,28 @@
+
+print(23%4)
+
+print(4%23)
+
+#floor division operator (//)
+print(23/4) #5.75
+print(23//4) #5
+print(60//7) #8
+print(60//6) #10
+
+#exponsel opretor``
+print(7**3) #343
+print(23>45)  # f
+print(23>=45) # f
+print(23<=45) # t
+print(23==45) # f
+print(23!=45) # t
+
+# string comparision
+print("appleeeee">"banana")
+print("apple">"apple")
+print("apple"=="apple")
+# on the basis of ASCII(American Stanterd code for intercha)
+print("oranGe"<"orange")
+print("ABCD"<"ABC")
+
+
