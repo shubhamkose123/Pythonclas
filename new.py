f41df82fe8f3eaa74889669d180faf29271dc9fe
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-12-26 14:17:23 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,15 @@
+# import first as f # aliyasing file ko
+
+# p=int(input("Enter number="))
+# q=int(input("Enter number="))
+# print(f.multi(p,q))
+
+
+# from first import sub as s # paricular function ko
+# print(s(10,7))
+
+
+from first import add as a
+data=a(10,5)
+print(data)
+#a(10,5)
